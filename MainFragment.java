db015b267c5626b6bbf4eb7e1bb07f13d2c4d5b6
SAI JAGADISH DARAPUREDDY <sai.jagadish.d@gmail.com>
2016-07-31 20:26:23 +0530

    Add files via upload

---
+++ MainFragment.java
@@ -0,0 +1,24 @@
+package com.example.admin.actionbar;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 7/31/2016.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+
+}
---
+++ TextFragment.java
@@ -0,0 +1,28 @@
+package com.example.admin.actionbar;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by admin on 7/31/2016.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class TextFragment  extends Fragment {
+    TextView text;
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.text_fragment, container, false);
+        text= (TextView) view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
+    }
+
+}
